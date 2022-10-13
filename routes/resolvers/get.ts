20ef7812bbc8401159a46401a7bfaf998ff5f7d5
Samet <hey@samet.codes>
2022-10-13 15:24:30 +0300

    returns subtitle data as calculating #7

--- routes/resolvers/get.ts
+++ routes/resolvers/get.ts
@@ -17,7 +17,7 @@ export const searchSubtitles = async (req: Request, res: Response) => {
     const defaultParams: ISearchParams = { order_by: "download_count", order_direction: "desc", languages: "en", type: "Movie" };
     const results = await OSAPI.search(Object.assign({}, defaultParams, req.query));
 
-    if(results instanceof Error) {
+    if (results instanceof Error) {
         return res.status(500).json({ error: results.message });
     }
 
@@ -31,12 +31,45 @@ export const getSubtitle = async (req: Request, res: Response) => {
     if (!subtitleRecord) {
         const jobs = await req.queues.subtitles.getJobs(["active", "waiting", "delayed", "paused"]);
         const isQueued = jobs.some(job => job.data.track_id === Number(req.params.track_id));
+
+        const defaultParams: ISearchParams = {
+            order_by: "download_count", order_direction: "desc",
+            languages: "en", type: "Movie", id: Number(req.params.track_id)
+        };
+        const results = await OSAPI.search(Object.assign({}, defaultParams, req.query));
+
+        if (results instanceof Error) return res.status(500).json({ error: results.message });
+        if (results.total_count <= 0) return res.status(404).json({ error: "Subtitle not found" });
+
+        const [data] = results.data;
+
+        const subtitle = {
+            ...data.attributes.feature_details,
+            track_id: data.attributes.feature_details.feature_id,
+            links: data.attributes.related_links,
+            opensubtitles_id: data.attributes.legacy_subtitle_id,
+            contentScore: "-",
+            lastUpdate: new Date()
+        };
+
         if (isQueued) {
-            return res.json({ status: "in-queue", message: "The movie is waiting in the queue" });
+            return res.json({
+                ...subtitle,
+                words: {
+                    completed: false, status: "queued", data: [],
+                    message: "The subtitle is in queue to be processed",
+                }
+            });
         }
 
         await req.queues.subtitles.add({ method: "calculateRarities", track_id: Number(req.params.track_id) });
-        return res.json({ status: "queued", message: "The movie added to the queue" })
+        return res.json({
+            ...subtitle,
+            words: {
+                completed: false, status: "queued", data: [],
+                message: "The subtitle added to the queue to be processed",
+            }
+        })
     }
 
     if (!(subtitleRecord?.lastUpdate) || subtitleRecord.lastUpdate < Date.now() - 1000 * 60 * 60 * 24) {
@@ -64,19 +97,24 @@ export const getSubtitle = async (req: Request, res: Response) => {
             }
         },
         { $sort: { rarityScore: -1 } },
-    ]).toArray()
+        ]).toArray()
 
-    const response = Object.assign({}, subtitleRecord, { words });
-    return res.json({ status: "calculated", response });
+    const response = Object.assign({}, subtitleRecord, {
+        words: {
+            completed: true, status: "completed", data: words,
+            message: "Word rarities was calculated.",
+        }
+    });
+    return res.json(response);
 }
 
 export const getSubtitles = async (req: Request, res: Response) => {
-    const { limit = 20, offset = 0, sort = {contentScore: -1} } = req.query;
+    const { limit = 20, offset = 0, sort = { contentScore: -1 } } = req.query;
     const subtitles = await req.database.collection('subtitles').find()
         .sort(sort as Sort)
         .limit(Number(limit))
         .skip(Number(offset))
         .toArray();
-    
+
     return res.json(subtitles);
 }
